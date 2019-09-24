53edd88be3bfb69cc0ad47b4f5c0eb628f1cd02b
yixinglu <2520865+yixinglu@users.noreply.github.com>
2019-09-24 11:30:46 +0800

    Parse test file

--- main/main.go
+++ main/main.go
@@ -4,7 +4,7 @@ import (
 	"flag"
 	"log"
 
-	"github.com/vesoft/nebula-test/parser"
+	"github.com/vesoft-inc/nebula-test/parser"
 )
 
 func main() {
--- parser/parser.go
+++ parser/parser.go
@@ -2,34 +2,87 @@ package parser
 
 import (
 	"bufio"
+	"bytes"
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"strings"
 )
 
 func ReadFile(filename string) error {
-	bytes, err := ioutil.ReadFile(filename)
+	b, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return err
 	}
 
-	scanner := bufio.NewScanner(strings.NewReader(string(bytes)))
+	scanner := bufio.NewScanner(strings.NewReader(string(b)))
+	// TODO(yee): Use FSM to implement parse
+	var inBuf, outBuf bytes.Buffer
+	var respResult, testName string
+	isInput, isOutput := false, false
+	const (
+		testPrefix = "=== test:"
+		inPrefix   = "--- in"
+		outPrefix  = "--- out"
+	)
 	for scanner.Scan() {
-		parse(scanner.Text())
+		text := scanner.Text()
+		if strings.HasPrefix(text, testPrefix) {
+			if isOutput {
+				diff(testName, outBuf.String(), respResult)
+				outBuf.Reset()
+				isOutput = false
+			}
+
+			// Reset test comment after last test output result
+			prefixLen := len(testPrefix)
+			if prefixLen > len(text) {
+				return errors.New(fmt.Sprintf("%s length is larger than %s", testPrefix, text))
+			}
+			testName = strings.TrimSpace(text[prefixLen:])
+		} else if strings.HasPrefix(text, inPrefix) {
+			isInput = true
+		} else if strings.HasPrefix(text, outPrefix) {
+			isOutput = true
+
+			if isInput {
+				request(inBuf.String())
+				isInput = false
+				inBuf.Reset()
+			}
+		} else {
+			if isInput {
+				inBuf.WriteString(text)
+			}
+			if isOutput {
+				outBuf.WriteString(text)
+			}
+		}
+	}
+
+	if isOutput {
+		diff(testName, outBuf.String(), respResult)
+		outBuf.Reset()
+		isOutput = false
 	}
 
 	return nil
 }
 
-// TODO(yee): Use FSM to implement parse
-func parse(text string) {
-	if strings.HasPrefix(text, "=== test") {
-		log.Println(text)
-	} else if text == "--- in" {
-		log.Println("in")
-	} else if text == "--- out" {
-		log.Println("out")
+// TODO(yee): connect nebula server and send gpl stmt
+func request(gql string) {
+	gql = strings.TrimSpace(gql)
+	log.Println(gql)
+}
+
+// TODO(yee): diff output result and response result
+func diff(testName, expected, real string) {
+	expected = strings.TrimSpace(expected)
+	real = strings.TrimSpace(real)
+	if expected != real {
+		log.Printf("expected: %s, real: %s", expected, real)
 	} else {
-		log.Println(text)
+		log.Printf("Test (%s) passed", testName)
 	}
 }
